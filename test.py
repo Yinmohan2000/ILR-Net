6e0df07ae5f45ea8c0b3bab4b76b1eed0015739b
Yinmohan2000 <148527773+Yinmohan2000@users.noreply.github.com>
2024-10-30 12:55:15 +0800

    test.py

--- test.py
+++ test.py
@@ -5,21 +5,21 @@ import numpy as np
 import cv2
 
 with torch.no_grad():
-    input = Image.open(r"D:\PycharmProjects\YCrBr\data\eval\eval15\low\LOL\1.png")
+    input = Image.open(r"...\...\...\...")
     input = np.asarray(input)
     input_c = input.copy()
     input = np.array(input, dtype="float32")/255.0
-    input = torch.from_numpy(input).view(1, 400,600, 3).cuda()  # 图像大小统一为600 * 400
+    input = torch.from_numpy(input).view(1, 400,600, 3).cuda()  # 图像大小手动设置为600 * 400
     print(input.shape)
     enhance_net = ILR().cuda()
-    enhance_net.load_state_dict(torch.load(r"D:\PycharmProjects\yinmohan\checkpoint\ILR_net_final.pth"))
-    #enhance_net.load_state_dict(torch.load(r"D:\PycharmProjects\yinmohan\checkpoint\ILR_net_final_1.pth"))
+    enhance_net.load_state_dict(torch.load(r"...\...\..."))
+    #enhance_net.load_state_dict(torch.load(r"...\...\..."))
     out_S = enhance_net(input)
     out_S = np.squeeze(out_S, axis=0)
     out_S = out_S.cpu().detach().numpy()
     out = out_S
     im = np.clip(out * 255.0, 0, 255.0).astype('uint8')
     im = im[:,:,[2, 1, 0]]
-    cv2.imwrite(r"D:\PycharmProjects\yinmohan\result\000.png", im)
+    cv2.imwrite(r"...\...\...", im)
 
 
