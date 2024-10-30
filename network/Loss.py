bc6966879d0a89bf5ea222dd47112bc9adacb295
Yinmohan2000 <148527773+Yinmohan2000@users.noreply.github.com>
2024-10-30 13:04:15 +0800

    Loss.py

--- network/Loss.py
+++ network/Loss.py
@@ -63,84 +63,6 @@ class DecomLoss(nn.Module):
             0.001*recon_loss_mutal_low + 0.001*recon_loss_mutal_high + \
             0.1*ismooth_loss_low + 0.1*ismooth_loss_high + \
             0.01*equal_r_loss
-# class RelightLoss(nn.Module):
-#     def __init__(self):
-#         super(RelightLoss, self).__init__()
-#     def forward(self, r_delta, l_low, input_high):
-#         # l_low_3 = torch.cat((l_low, l_low, l_low), -1)
-#         relight_loss = torch.mean(torch.abs(l_low * r_delta - input_high))
-#         ismooth_loss_delta = smooth(r_delta, l_low)
-#         # ssim_loss = 1 - pytorch_ssim.SSIM(window_size=self.window_size)(l_delta, input_high)
-#         # mse_loss  = nn.MSELoss(l_delta,input_high)
-#         return  relight_loss + 3 * ismooth_loss_delta
-# class L_spa(nn.Module):
-#     def __init__(self):
-#         super(L_spa, self).__init__()
-#         # print(1)kernel = torch.FloatTensor(kernel).unsqueeze(0).unsqueeze(0)
-#         kernel_left = torch.FloatTensor([[0, 0, 0], [-1, 1, 0], [0, 0, 0]]).cuda().unsqueeze(0).unsqueeze(0)
-#         kernel_right = torch.FloatTensor([[0, 0, 0], [0, 1, -1], [0, 0, 0]]).cuda().unsqueeze(0).unsqueeze(0)
-#         kernel_up = torch.FloatTensor([[0, -1, 0], [0, 1, 0], [0, 0, 0]]).cuda().unsqueeze(0).unsqueeze(0)
-#         kernel_down = torch.FloatTensor([[0, 0, 0], [0, 1, 0], [0, -1, 0]]).cuda().unsqueeze(0).unsqueeze(0)
-#         self.weight_left = nn.Parameter(data=kernel_left, requires_grad=False)
-#         self.weight_right = nn.Parameter(data=kernel_right, requires_grad=False)
-#         self.weight_up = nn.Parameter(data=kernel_up, requires_grad=False)
-#         self.weight_down = nn.Parameter(data=kernel_down, requires_grad=False)
-#         self.pool = nn.AvgPool2d(3,1,1)
-#     def forward(self, org, enhance):
-#         b, c, h, w = org.shape
-#         org_mean = torch.mean(org, 1, keepdim=True)
-#         enhance_mean = torch.mean(enhance, 1, keepdim=True)
-#         org_pool = self.pool(org_mean)
-#         enhance_pool = self.pool(enhance_mean)
-#         weight_diff = torch.max(
-#             torch.FloatTensor([1]).cuda() + 10000 * torch.min(org_pool - torch.FloatTensor([0.3]).cuda(),
-#                                                               torch.FloatTensor([0]).cuda()),
-#             torch.FloatTensor([0.5]).cuda())
-#         E_1 = torch.mul(torch.sign(enhance_pool - torch.FloatTensor([0.5]).cuda()), enhance_pool - org_pool)
-#         D_org_letf = F.conv2d(org_pool, self.weight_left, padding=1)
-#         D_org_right = F.conv2d(org_pool, self.weight_right, padding=1)
-#         D_org_up = F.conv2d(org_pool, self.weight_up, padding=1)
-#         D_org_down = F.conv2d(org_pool, self.weight_down, padding=1)
-#         D_enhance_letf = F.conv2d(enhance_pool, self.weight_left, padding=1)
-#         D_enhance_right = F.conv2d(enhance_pool, self.weight_right, padding=1)
-#         D_enhance_up = F.conv2d(enhance_pool, self.weight_up, padding=1)
-#         D_enhance_down = F.conv2d(enhance_pool, self.weight_down, padding=1)
-#         D_left = torch.pow(D_org_letf - D_enhance_letf, 2)
-#         D_right = torch.pow(D_org_right - D_enhance_right, 2)
-#         D_up = torch.pow(D_org_up - D_enhance_up, 2)
-#         D_down = torch.pow(D_org_down - D_enhance_down, 2)
-#         E = (D_left + D_right + D_up + D_down)
-#         # E = 25*(D_left + D_right + D_up +D_down)
-#         return E
-# class L_exp(nn.Module):
-#     def __init__(self, patch_size, mean_val):
-#         super(L_exp, self).__init__()
-#         # print(1)
-#         self.pool = nn.AvgPool2d(patch_size)
-#         self.mean_val = mean_val
-#
-#     def forward(self, x):
-#         b, c, h, w = x.shape
-#         x = torch.mean(x, 1, keepdim=True)
-#         mean = self.pool(x)
-#         d = torch.mean(torch.pow(mean - torch.FloatTensor([self.mean_val]).cuda(), 2))
-#         return d
-# class L_color(nn.Module):
-#     def __init__(self):
-#         super(L_color, self).__init__()
-#
-#     def forward(self, x):
-#         b, c, h, w = x.shape
-#         mean_rgb = torch.mean(x, [2, 3], keepdim=True)
-#         mr = mean_rgb[:,0:1,:,:]
-#         mg = mean_rgb[:, 1:2, :, :]
-#         mb = mean_rgb[:, 2:3, :, :]
-#         # mr, mg, mb = torch.split(mean_rgb, 1, dim=1)
-#         Drg = torch.pow(mr - mg, 2)
-#         Drb = torch.pow(mr - mb, 2)
-#         Dgb = torch.pow(mb - mg, 2)
-#         k = torch.pow(torch.pow(Drg, 2) + torch.pow(Drb, 2) + torch.pow(Dgb, 2), 0.5)
-#         return k
 class CharbonnierLoss(torch.nn.Module):
     def __init__(self, epsilon=1e-6):
         super(CharbonnierLoss, self).__init__()
@@ -171,7 +93,6 @@ def vgg16_loss(feature_module,loss_func,y,y_):
     out_=feature_module(y_)
     loss=loss_func(out,out_)
     return loss
-# 获取指定的特征提取模块
 def get_feature_module(layer_index,device=None):
     vgg = vgg16(pretrained=True, progress=True).features
     vgg.eval()
@@ -216,5 +137,5 @@ class ssim(nn.Module):
 
 # if __name__ == '__main__':
 #     tensor = torch.rand(1, 300, 400, 1)
-#     out_data = smooth(tensor, torch.rand(1, 300, 400, 3))
+#     out_data = ssim(tensor, torch.rand(1, 300, 400, 3))
 #     print(out_data)
