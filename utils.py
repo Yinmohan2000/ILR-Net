a295f3e4ef22ad3c81a6c35c422f6d3b5ad8eaf9
Yinmohan2000 <148527773+Yinmohan2000@users.noreply.github.com>
2024-10-30 13:01:11 +0800

    utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,5 @@
 import numpy as np
 from PIL import Image
-
-
 def data_augmentation(image, mode):
     if mode == 0:
         # original
@@ -30,22 +28,18 @@ def data_augmentation(image, mode):
         # rotate 270 degree and flip
         image = np.rot90(image, k=3).copy()
         return np.flipud(image).copy()
-
-
+        
 def load_images(file):
     im = Image.open(file)
     return np.array(im, dtype="float32") / 255.0
-
-
+    
 def save_images(filepath, result_1, result_2=None):
     result_1 = np.squeeze(result_1)
     result_2 = np.squeeze(result_2)
-
     if not result_2.any():
         cat_image = result_1
     else:
         cat_image = np.concatenate([result_1, result_2], axis=1)
-
     im = Image.fromarray(np.clip(cat_image * 255.0, 0, 255.0).astype('uint8'))
     im.save(filepath, 'png')
 
